84c0f0b30199129c9af5e0858d1405e967dc535e
teopteop <teopteop0718@gmail.com>
2025-04-30 14:43:24 +0900

    삭제기능 추가

--- src/challenge/lv2/Kiosk.java
+++ src/challenge/lv2/Kiosk.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
@@ -37,23 +38,23 @@ public class Kiosk {
                 System.out.println("[ " + chooseMenu.getCategory().toUpperCase() + " MENU ]");
 
                 //menuItem 조회 forEach만 사용시 인덱스에 접근 불가능 -> IntStream 사용
-                Consumer<List<MenuItem>> m1 = menuItemList -> {
+                Consumer<List<MenuItem>> myConsumer1 = menuItemList -> {
                     IntStream.range(0, menuItemList.size()).forEach(i -> {
                         MenuItem m = menuItemList.get(i);
                         System.out.println((i+1) + ". " + m.toString());
                     });
                 };
-                m1.accept(chooseMenu.getMenuItemList());
+                myConsumer1.accept(chooseMenu.getMenuItemList());
                 System.out.println("0.  뒤로가기");
 
                 chooseMenuItem = chooseMenu.getMenuItem(getInput(1, chooseMenu.getMenuItemList().size()));
 
                 //선택한 item 출력
-                Consumer<MenuItem> m2 = (m) -> {
+                Consumer<MenuItem> myConsumer2 = (m) -> {
                     System.out.println("당신이 선택한 메뉴: " + m.toString() +
                             "\n위 메뉴를 추가하시겠습니까?\n1. 확인  |  2. 취소");
                 };
-                m2.accept(chooseMenuItem);
+                myConsumer2.accept(chooseMenuItem);
                 order.addOrder(getInput(1, 2), chooseMenuItem);
                 continue;
             } else if (choice == menus.size() + 1) {
@@ -68,12 +69,22 @@ public class Kiosk {
                 System.out.println("할인 정보를 입력해주세요.\n" + "1. 국가유공자  :  10% \n" +
                         "2. 군인  :  5%\n" + "3. 학생  :  3%\n" + "4. 일반  :  0%");
                 Grade grade = Grade.gradeChoice(getInput(1, 4));
-                System.out.println("주문이 완료되었습니다. 금액은 W " +
-                        grade.discountPrice(order.getTotalPrice()) + " 입니다.");
+                //
+                System.out.printf("주문이 완료되었습니다. 금액은 W %.2f 입니다.\n",
+                        grade.discountPrice(order.getTotalPrice()));
                 break;
             } else if(choice == 2) {
                 //삭제 기능 적용
-
+                BiFunction<String, List<MenuItem>, Boolean> myFunction1 = (inputString, orders) ->
+                    orders.removeIf(m -> inputString.equals(m.getItemName()));
+                sc.nextLine();//다음입력이 nextLine 이기에 버퍼 비우기
+                while (true) {
+                        System.out.println("삭제할 메뉴의 이름을 입력하세요.(대소문자 구분)");
+                        if(myFunction1.apply(sc.nextLine(), order.getOrders())){
+                            System.out.println("삭제 완료");
+                            break;
+                        } System.out.println("입력한 메뉴명을 확인해주세요.");
+                }
             }
         }
     }
